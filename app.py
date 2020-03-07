7f0336b5ebd4f35ce09c1f2376475678fd563233
ybacoder <ybacoder@gmail.com>
2020-03-07 13:32:47 -0600

    Add starter code for routes

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", )
+
+
+@app.route("/data")
+def data_access():
+    return render_template("data.html", )
+
+
+@app.route("/plot")
+def plot():
+    return render_template("plot.html", )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
