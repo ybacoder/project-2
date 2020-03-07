44da4084c41b3046c210a6eac09e5ab2e5b38ba6
ybacoder <ybacoder@gmail.com>
2020-03-07 13:44:44 -0600

    Add scrape route

--- app.py
+++ app.py
@@ -17,5 +17,10 @@ def plot():
     return render_template("plot.html", )
 
 
+@app.route("/scrape")
+def scrape():
+    return render_template("scrape.html", )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
