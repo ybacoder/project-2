76b0354beaaf35f33e1e455c76cfffc2e764b866
Allie K <allie@Allisons-MBP.lan1>
2020-03-28 09:36:25 -0500

    Added database and models

---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import warnings
+from sqlalchemy.exc import ProgrammingError 
+
+
+USER = "k2u1yqycv3bodj4z"
+PASSWORD = "l2gzxi58ynrq4pbd"
+HOST = "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+PORT = "3306"
+DATABASE = "pmfkjy5n1gcwlin1"
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+try:
+    engine.execute(f"CREATE DATABASE {DATABASE}")
+except ProgrammingError:
+    warnings.warn(
+        f"Could not create database {DATABASE}. Database {DATABASE} may already exist."
+    )
+    pass
+
+
+engine.execute(f"USE {DATABASE}")
---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from database import Base
+import datetime
+
+
+class DictMixIn:
+    def to_dict(self):
+        return {
+            column.name: getattr(self, column.name)
+            if not isinstance(
+                getattr(self, column.name), (datetime.datetime, datetime.date)
+            )
+            else getattr(self, column.name).isoformat()
+            for column in self.__table__.columns
+        }
+
+
+class Wind(Base):
+    __tablename__ = "wind_data"
+
+    id = Column(Integer, primary_key=True, index=True)
+    lambda_filename = Column(String(250), nullable=True)
+    lambda_file_url = Column(String(250), nullable=True)
+    wind_filename = Column(String(250), nullable=True)
+    wind_file_url = Column(String(250), nullable=True)
+    SCEDTimeStamp = Column(DateTime, nullable=True)
+    SystemLambda = Column(Float, nullable=True)
+    RepeatedHourFlag = Column(String(250), nullable=True)
+    System_Wide = Column(Float, nullable=True)
+    LZ_South_Houston = Column(Float, nullable=True)
+    LZ_West = Column(Float, nullable=True)
+    LZ_North = Column(Float, nullable=True)
+    DSTFlag = Column(String(250), nullable=True)
