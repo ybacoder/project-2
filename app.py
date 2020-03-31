7df7a8c39cd6fa8b1160a478f83bcc2648480c13
Eric Nordstrom <eanord4@gmail.com>
2020-03-30 19:49:02 -0500

    Add hopefully working 'since' query

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, _app_ctx_stack, url_for
 import scraping
 import pandas as pd
-import scraping as dt
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import scoped_session
 from sqlalchemy import create_engine, func
@@ -34,32 +34,77 @@ def home():
 def data_access():
     """return a JSON of all stored data"""  # doesn't make a lot of sense. adding filtering here (or sub-endpoints like "today" or "latest") is a subject of future work.
 
-    query = session.query(Data)  # query all rows and columns
-    df = pd.read_sql(query.statement, con=engine)
+    # query = session.query(Data)  # query all rows and columns
+    # df = pd.read_sql(query.statement, con=engine)
 
-    return jsonify(df.to_dict)
+    return render_template("data.html")
 
 
 # non-time data vs. time data
 @app.route("/timeseries")
 def plot1():
-    return render_template("plot1.html", )
+    """Return all timeseries data"""
 
+    results = app.session.query(models.Wind)\
+        .filter(models.Wind.System_Wide != 0)\
+        .all()
+
+    trace1 = {
+        "x": [], # Erin to fill in
+        "y": [], # Erin to fill in
+        "type": "scatter"
+    }
+
+    trace2 = {
+        "x": [], # Erin to fill in
+        "y": [], # Erin to fill in
+        "type": "scatter"
+    }
+
+    layout = {
+      "title": "Wind Generation and System Lambda",
+      "height": 700,
+      }
+    
+    return render_template("plot1.html", trace=[trace1, trace2], layout=layout)
 
 # non-time data vs. non-time data
-@app.route("/correlation/<query>")
-def plot2(query):
+@app.route("/correlation")
+def plot2():
+    """Return all non-zero non-timeseries data"""
 
-    ### per Erin's testing specifications
+    results = app.session.query(models.Wind)\
+        .filter(models.Wind.System_Wide != 0)\
+        .all()
+
+    trace = {
+        "x": [result.System_Wide for result in results],
+        "y": [result.SystemLambda for result in results],
+        "mode": "markers",
+        "type": "scatter"
+    }
+
+    layout = {
+      "title": "Wind Generation vs. System Lambda",
+      "xaxis": {
+          "title": "Wind Generation"
+      },
+      "yaxis": {
+          "title": "System Lambda"
+      },
+      "height": 700,
+      }
     
-    return render_template("plot2.html", )
+    return render_template("plot2.html", trace=trace, layout=layout)
 
 
 @app.route("/scrape")
 def scrape():
 
     # get "since" date to avoid longer-than-necessary scrapes
-    since = app.session.query(models.Wind.SCEDTimeStamp).last()[0]
+    since = app.session.query(models.Wind.SCEDTimeStamp)[-1][0]
+    print(since, since[0], sep='\n')
+    input()
 
     # scrape
     clean_data.data_scrape(since)
