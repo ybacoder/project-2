ec36a2d3bb5e9b235e10a349365a9f92bb158e77
Eric Nordstrom <eanord4@gmail.com>
2020-04-09 21:14:51 -0500

    Testing if getting the result of the worker job is as easy as 'job.result' (I suspect not)

--- app.py
+++ app.py
@@ -273,7 +273,9 @@ def scrape():
     since = app.session.query(models.Wind.SCEDTimeStamp)[-1][0]
 
     # scrape & munge
-    queue.enqueue(clean_data.data_scrape, since, job_timeout=1200)  # 20 minutes
+    job = queue.enqueue(clean_data.data_scrape, since, job_timeout=1200)  # 20 minutes
+
+    print(job.result)  ### test
 
     # load into db
     queue.enqueue(load.csv_db)
